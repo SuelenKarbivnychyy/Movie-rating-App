bf271ea1c7502ba5b106cb79abe17cf0886e3130
suelenk <suelenmatosr@gmail.com>
2022-10-29 13:05:07 -0400

    started part 2

--- crud.py
+++ crud.py
@@ -1,15 +1,40 @@
 """CRUD operations."""
 
 from model import db, User, Movie, Rating, connect_to_db
+from datetime import datetime
 
 
 
 def create_user(email, password):
     """Create and return a new user."""
 
-    user = User(email='dimma@gmail.com', password='123')
+    user = User(
+            email=email, 
+            password=password)
 
     return user
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(
+                title=title, 
+                overview=overview,
+                release_date = release_date, 
+                poster_path = poster_path)  
+
+    return movie  
+
+
+
+def create_rating(user, movie, score):
+    '''Create and return a new score'''  
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+
+    return rating
 
 
 if __name__ == '__main__':
